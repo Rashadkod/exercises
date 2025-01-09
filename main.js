7b801316bd1847fee88127409f9de283ab453c88
Rashadkod <rashad.hasanov0@gmail.com>
2025-01-09 13:40:28 +0400

    // bezi sintaksis sehvler aradan qaldirildi

---
+++ main.js
@@ -0,0 +1,11 @@
+document.write("<h1>Tapsiriq 2</h1>");
+document.write(`<p>Ixtiyari mətn ilə paraqraf</p>`);
+document.write(`
+    <ul>
+        <li>Siyahı elementi 1</li>
+        <li>Siyahı elementi 2</li>
+        <li>Siyahı elementi 3</li>
+    </ul>
+`);
+
+// bezi sintaksis sehvler aradan qaldirildi
