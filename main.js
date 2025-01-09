5267fa4670697d9d251ff3181a7ed8f1554a039e
Rashadkod <rashad.hasanov0@gmail.com>
2025-01-09 13:55:16 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.write(`
+  <h1>Tapşiriq 3</h1>
+  <p>İxtiyari mətn ilə paraqraf</p>
+  <ol>
+    <li>Siyahi elementi 1 və <a href="#">link</a></li>
+    <li>Siyahi elementi 2</li>
+    <li>Siyahi elementi 3</li>
+  </ol>
+`);
